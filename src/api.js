d3caffd0505c2c07599260cc3731891512d97636
Arun <arun.@Aruns-MacBook-Pro-2.local>
2025-04-28 23:47:17 +0530

    done

--- src/api.js
+++ src/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE_URL = "http://localhost:3000";
+const API_BASE_URL = "https://edu-cational-backend.onrender.com";
 
 export const signup = async (userData) => {
     return await axios.post(`${API_BASE_URL}/signup`, userData, { withCredentials: true });
